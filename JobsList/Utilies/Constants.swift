7bfb152e6a9d84e69153f5b23ec0e7afd404dcb5
bshayer <bshayerr77@gmail.com>
2018-04-18 09:07:48 +0300

    1st

---
+++ JobsList/Utilies/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  
+//
+//  Created by rihab aldabbagh on 20/01/2018.
+//  Copyright © 2018 ohoud. All rights reserved.
+//
+
+import Foundation
+
+
+// Segues
+
+let TO_JOBS = "toJobs"
+let TO_JOBS_DETAILS  = "toJobDetails"
+
